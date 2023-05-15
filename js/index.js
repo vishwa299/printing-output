1b644b72226c576245bf38e3c77ffc3f7270f517
vishwa299 <129430690+vishwa299@users.noreply.github.com>
2023-05-15 09:38:27 -0400

    Create index.js
    JavaScript file created and JS content added.

---
+++ js/index.js
@@ -0,0 +1,52 @@
+// TASK 1
+let a = 3;
+let b = 5;
+let c;
+
+// alert("description of alerted_value: " + alerted_value);
+let output = `
+    let a = 3;
+    let b = 5;
+    let c;
+    --------------------
+    a + b = ${a + b}
+    a - b = ${a - b}
+    a * b = ${a * b}
+    a / b = ${a / b}
+    a % b = ${a % b}
+    a += b = ${a += b}
+    a -= b = ${a -= b}
+    a *= b = ${a *= b}
+    a /= b = ${a /= b}
+    a %= b = ${a %= b}
+    a == b = ${a == b}
+    a != b = ${a != b}
+    a > b = ${a > b}
+    a < b = ${a < b}
+    !a && !c = ${!a && !c}
+    !a || !c = ${!a || !c}
+    `;
+alert(output);
+/*
+a + b
+a - b
+a * b
+a / b
+a % b
+a += b
+a -= b
+a *= b
+a /= b
+a %= b
+a == b
+a != b
+a > b
+a < b
+!a && !c 
+!a || !c 
+*/
+
+
+
+//TASK 2
+// to be done here ...
