676796e5bdef3c1909dc4bbbef944c7fca580c5d
vishwa299 <129430690+vishwa299@users.noreply.github.com>
2023-05-15 20:31:48 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -27,6 +27,8 @@ let output = `
     !a || !c = ${!a || !c}
     `;
 alert(output);
+
+
 /*
 a + b
 a - b
@@ -49,4 +51,18 @@ a < b
 
 
 //TASK 2
-// to be done here ...
+// Declare the variable first_name and assign it with the value of your first name;
+// Declare the variable last_name and assign it with the value of your last name;
+// Declare the variable email and assign it with the value of your email;
+// Declare the variable output;
+// Create the expression concatenating variables first_name, last_name, email and string literals needed to complete the sentence reading: "My name is Your-first-name Your-last-name. You can contact me at your-email@mail.com.";
+// Assign output with gotten expression;
+// Alert output.
+
+
+let first_name = "Vishwa";
+let last_name = "Shah";
+let email = "shah0368@algonquinlive.com";
+output = "My name is " + first_name + " " + last_name + ". You can contact me at " + email + ".";
+alert(output);
+
